0433f70ce257eda5e0546e8470e1c31b8e827db2
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-06-15 08:33:24 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,40 @@
 
+#importing libraries
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as sp
+import numpy as np
+from collections import OrderedDict
+from plotly.subplots import make_subplots
+from streamlit_option_menu import option_menu
+import plotly
+import pandas as pd
+import plotly.figure_factory as ff
 import streamlit as st
+import matplotlib.pyplot as plt
+import altair as alt
+from datetime import datetime
+import datetime as dt 
+import calendar
+import time
+from PIL import Image
+from dateutil import relativedelta
+import re
+from collections import defaultdict
+from dateutil.relativedelta import relativedelta
+import io
+import msoffcrypto
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from deta import Deta
 
-def write():
-    st.title("Home Page")
-    st.write("Welcome to the Home Page!")
+#debug 30th March 2024 to remove waring and future downcasting 
+
+pd.set_option('future.no_silent_downcasting', True)
+
+
+#Set page layout here
+st.set_page_config(layout="wide")
